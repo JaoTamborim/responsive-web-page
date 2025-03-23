6ed80a217e500429da3ed9f74482ec9b2d1fb8b8
João <jaotamborim@gmail.com>
2025-03-23 17:24:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let lastScrollTop = 0;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (currentScroll > lastScrollTop) {
+        header.style.top = "-80px"; 
+    } else {
+        header.style.top = "0";
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
